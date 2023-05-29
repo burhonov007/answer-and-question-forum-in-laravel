f89dce2cacb192021a6a6f79aa6ed027f21be782
burkhonov007 <akmalburhonov5515@gmail.com>
2023-05-29 21:39:31 +0300

    shange like button

--- resources/views/inc/answer-btn-group.blade.php
+++ resources/views/inc/answer-btn-group.blade.php
@@ -1,20 +1,25 @@
 <div class="btn-group" role="group" style="margin-bottom: 10px;">
     <form action="{{ route('answers-like', $answer->id) }}" method="POST" style="margin-right: 10px;">
         @csrf
-        <button type="submit" class="btn btn-primary rounded"><i class="fas fa-thumbs-up"></i>
+        <button type="submit" id="like-button" class="btn {{ $answer->likeByUser(\Auth::id()) ?  'btn-success' : 'btn-outline-success' }} rounded"><i class="fas fa-thumbs-up"></i>
             {{ $answer->likes->count() }}
         </button>
     </form>
     <form action="{{ route('answers-dislike', $answer->id) }}" method="POST" style="margin-right: 10px;">
         @csrf
-        <button type="submit" class="btn btn-danger rounded"><i class="fas fa-thumbs-down"></i>
+        <button type="submit" class="btn {{ $answer->dislikeByUser(\Auth::id()) ?  'btn-danger' : 'btn-outline-danger' }}
+         rounded"><i class="fas fa-thumbs-down"></i>
             {{ $answer->dislikeCount() }}
         </button>
     </form>
     <form action="{{ route('show-comments' , $answer->id) }}" method="GET" style="margin-right: 10px;">
         @csrf
-        <button type="submit" class="btn btn-secondary rounded"><i class="fas fa-comments"></i>
+        <button type="submit" class="btn btn-secondary rounded" style="color: white"><i class="fas fa-comments"></i>
             {{ $answer->commentCount() }}
         </button>
     </form>
 </div>
+
+
+
+
